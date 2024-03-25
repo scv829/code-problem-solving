ecaa65d9414bc701460ba2b90144fe3b62e9f594
scv829 <53394239+scv829@users.noreply.github.com>
2024-03-25 18:41:40 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 564 ms, Memory: 19756 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        for(int i = x-1; i > 0; i--){
+            for(int j = 0; j < x - i - 1; j++) System.out.print(" ");
+            for(int k = 0; k < (2 * i) + 1 ; k++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < x - i - 1; j++) System.out.print(" ");
+            for(int k = 0; k < (2 * i) + 1 ; k++) System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}
