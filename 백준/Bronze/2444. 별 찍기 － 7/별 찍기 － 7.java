24f71d3da1bfe71a6f9fb1d5658c228cb17b94ad
scv829 <53394239+scv829@users.noreply.github.com>
2024-05-26 23:49:40 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 524 ms, Memory: 20964 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int N= scanner.nextInt();
+
+        for(int i = 1; i <= N; i++ ){
+            for(int first = i; first < N; first++) System.out.print(" ");
+            for(int middle = 0; middle < 2*i-1; middle++ ) System.out.print("*");
+            System.out.println();
+        }
+        for(int i = 1; i <= N; i++ ){
+            for(int first = 0; first < i; first++) System.out.print(" ");
+            for(int middle = 0; middle < (2*N)-(2*i+1); middle++) System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}
+
