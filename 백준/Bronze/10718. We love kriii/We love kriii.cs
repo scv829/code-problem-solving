a7be81e2b20fc1a6a2206c9a724dbf1620ddbf9f
scv829 <53394239+scv829@users.noreply.github.com>
2024-08-10 12:43:29 +0900

    [Bronze V] Title: We love kriii, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace code
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군\n강한친구 대한육군");
+        }
+    }
+}
