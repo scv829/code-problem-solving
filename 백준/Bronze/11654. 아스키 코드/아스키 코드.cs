95d609af079a50cfb7f1013c8d3fd1463a7c1ca8
scv829 <53394239+scv829@users.noreply.github.com>
2024-08-16 18:06:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.Read().ToString()));
+        }
+    }
+}
+
