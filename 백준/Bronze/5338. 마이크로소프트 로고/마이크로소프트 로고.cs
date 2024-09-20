a84ad86bfc684fcc2d08bce9e0f5afd725529558
scv829 <53394239+scv829@users.noreply.github.com>
2024-09-21 03:52:10 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       _.-;;-._");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-;;-._|");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-''-._|");
+        }
+    }
+}
