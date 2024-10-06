b731295b6c365d5541b24dec1e2dbbd8a9c347e4
scv829 <53394239+scv829@users.noreply.github.com>
2024-10-06 20:58:10 +0900

    [Bronze IV] Title: 베라의 패션, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+
+
+            int N = int.Parse(sr.ReadLine());
+
+            sb.AppendLine($"{N * (N-1)}");
+
+            sw.WriteLine(sb.ToString());
+
+            sr.Close();
+            sw.Close();
+
+        }
+    }
+}
