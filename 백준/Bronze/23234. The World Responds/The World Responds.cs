65cec0ce6d9068c179734d49bfe0f9ffec5531cb
scv829 <53394239+scv829@users.noreply.github.com>
2024-10-17 23:58:05 +0900

    [Bronze V] Title: The World Responds, Time: 44 ms, Memory: 5356 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("The world says hello!");
+
+            sw.Write(sb.ToString());
+
+            sr.Close();
+            sw.Close();
+
+        }
+    }
+}
