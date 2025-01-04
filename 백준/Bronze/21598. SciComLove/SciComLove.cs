a7c9c762b26d2346b1c352de9f91e65f5359f802
scv829 <53394239+scv829@users.noreply.github.com>
2025-01-04 13:33:18 +0900

    [Bronze V] Title: SciComLove, Time: 52 ms, Memory: 5496 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+            int N = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < N; i++) { sb.AppendLine("SciComLove"); }
+
+            sw.WriteLine(sb.ToString());
+
+            sr.Close();
+            sw.Close();
+
+        }
+    }
+}
