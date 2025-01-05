6cc881e1ab6897d2f9a96e745012c526053c9ce6
scv829 <53394239+scv829@users.noreply.github.com>
2025-01-05 22:19:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("2025-01-05");
+
+        }
+    }
+}
