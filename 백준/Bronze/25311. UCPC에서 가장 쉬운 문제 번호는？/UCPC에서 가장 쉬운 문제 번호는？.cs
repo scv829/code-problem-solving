49651aa717374c235577b337a42a95bea55b0ff4
scv829 <53394239+scv829@users.noreply.github.com>
2025-02-14 20:15:28 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("A");
+
+            sw.WriteLine(sb.ToString());
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
