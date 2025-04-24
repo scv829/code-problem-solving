b4fa50e5a9267d33c2130d2592e751b289c98739
scv829 <53394239+scv829@users.noreply.github.com>
2025-04-24 15:07:24 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 52 ms, Memory: 5636 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+namespace code
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+
+            int[] inputs = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+
+            sb.Append(inputs.Min());
+
+            sw.WriteLine(sb.ToString());
+
+            sw.Close();
+            sr.Close();
+        }
+    }
+
+}
